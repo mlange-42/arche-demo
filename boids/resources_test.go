971baa6de91bc29c62e3e86745fe9fcbca4b73e5
mlange-42 <martin_lange_@gmx.net>
2024-01-27 20:48:14 +0100

    add tests for acceleration grid

--- boids/resources_test.go
+++ boids/resources_test.go
@@ -12,4 +12,30 @@ func TestGrid(t *testing.T) {
 
 	assert.Equal(t, 8, grid.Cols)
 	assert.Equal(t, 4, grid.Rows)
+
+	pos := boids.Position{X: 1, Y: 1}
+	vel := boids.Velocity{X: 2, Y: 2}
+
+	grid.Add(1, 2, &pos, &vel)
+
+	assert.Equal(t, []boids.GridEntry{{pos.X, pos.Y, vel.X, vel.Y}}, grid.Get(1, 2))
+	assert.Equal(t, []boids.GridEntry{}, grid.Get(2, 1))
+
+	grid.Add(1, 2, &pos, &vel)
+	grid.Add(1, 2, &pos, &vel)
+	grid.Add(1, 2, &pos, &vel)
+	grid.Add(1, 2, &pos, &vel)
+	grid.Add(1, 2, &pos, &vel)
+	grid.Add(1, 2, &pos, &vel)
+	grid.Add(1, 2, &pos, &vel)
+
+	assert.Panics(t, func() {
+		grid.Add(1, 2, &pos, &vel)
+	})
+
+	assert.Equal(t, 8, len(grid.Get(1, 2)))
+	assert.Equal(t, 0, len(grid.Get(1, 3)))
+
+	grid.Clear()
+	assert.Equal(t, 0, len(grid.Get(1, 2)))
 }
