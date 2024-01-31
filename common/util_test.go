1ddfe606179b0652e48bc8515e5719100aca8c26
mlange-42 <martin_lange_@gmx.net>
2024-01-31 21:56:49 +0100

    move tests out of test package

--- common/util_test.go
+++ common/util_test.go
@@ -1,16 +1,15 @@
-package common_test
+package common
 
 import (
 	"testing"
 
-	"github.com/mlange-42/arche-demo/common"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRandBetweenUInt8(t *testing.T) {
 	counts := make([]int, 10)
 	for i := 0; i < 1000; i++ {
-		counts[common.RandBetweenUIn8(5, 3)]++
+		counts[RandBetweenUIn8(5, 3)]++
 	}
 
 	assert.Equal(t, 0, counts[2])
